5247e446bc3180c5c091ce9f7cb6f1b305a50f6b
Antony Santero <antony_santero@hotmail.com>
2022-10-03 13:30:32 +0200

    First commit

---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+})
+export class DetailPage implements OnInit {
+  
+  uneleve = {}  
+
+  constructor( private router: Router, private activeRoute : ActivatedRoute) {
+   this.activeRoute.queryParams.subscribe(params=>{
+      this.uneleve = this.router.getCurrentNavigation().extras.state.param1;
+     })
+   }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router,NavigationExtras} from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+   lstfilm:any;
+  constructor(private router: Router) {
+    fetch('./assets/films-json.json').then(res => res.json())
+    .then(json => {
+      this.lstfilm = json;
+    });
+  }
+  
+  affDetail(item){
+    let navigationExtras: NavigationExtras = {
+      state : {
+        param1: item
+        }
+      };
+    this.router.navigate(['/detail'],navigationExtras);
+    //ouvrir page detail
+    //envoyer le param a la page detail 
+  }
 }
